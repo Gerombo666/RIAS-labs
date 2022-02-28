9c4b3c8b4d7a16e1372afad5944018039e49f34a
Gerombo666 <44369699+Gerombo666@users.noreply.github.com>
2022-02-28 10:42:28 +0300

    Add Lab_01

---
+++ Lab_01/Lab_04/src/Client2.java
@@ -0,0 +1,10 @@
+public class Client2 {
+    
+    public static String ipAddr = "localhost";
+    public static int port = 1502;
+
+    public static void main(String[] args)
+    {
+        new ClientSomthing(ipAddr, port);
+    }
+}
